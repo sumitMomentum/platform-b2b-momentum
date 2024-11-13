c740f68091f67c5ce8f7a054d10f10bd3020a43e
Himal-Momentum-E <himal@momentum-e.com>
2024-11-13 10:54:10 +0530

    alligned cclerk auth

--- src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,5 +2,14 @@ import { SignIn } from "@clerk/nextjs";
 import { Box } from "@mui/material";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh" // Full viewport height to center vertically
+    >
+      <SignIn />
+    </Box>
+  );
 }
--- src/app/[locale]/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/[locale]/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,13 @@ import { Box } from "@mui/material";
 
 export default function Page() {
   return (
-    
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh" // Full viewport height to center vertically
+    >
       <SignUp />
+    </Box>
   );
 }
