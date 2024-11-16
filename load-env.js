b68879c28f2f6bf960ab4a66ebd9552e5d4c0cfb
Himal-Momentum-E <himal@momentum-e.com>
2024-11-17 03:57:53 +0530

    minute changes

--- load-env.js
+++ load-env.js
@@ -1,3 +1,5 @@
+const chalk = require("chalk");
+const { error } = require("console");
 const fs = require("fs");
 const path = require("path");
 require("dotenv").config({ path: ".env" });
@@ -6,8 +8,24 @@ const env = process.env.NODE_ENV || "development";
 const envFilePath = path.resolve(process.cwd(), `.env.${env}`);
 
 if (fs.existsSync(envFilePath)) {
-  console.log(`Loading environment variables from: ${envFilePath}`);
+  const envColor =
+    {
+      local: chalk.blue,
+      development: chalk.green,
+      production: chalk.yellow,
+      test: chalk.red,
+    }[env] || chalk.white;
+
+  console.log(envColor(`🌍 Environment: ${env}`)); // Log the environment with an icon
+  console.log(
+    chalk.cyan(
+      `📄 Loading environment variables from: ${envFilePath
+        .split(path.sep)
+        .pop()}`
+    )
+  ); // Log the file name with a different color and icon
+
   require("dotenv").config({ path: envFilePath });
 } else {
-  console.error(`Environment file not found: ${envFilePath}`);
+  console.error(chalk.red(`Environment file not found: ${envFilePath}`));
 }
