4847c4c4f0b287d31f79b790d28ef587102870d9
nishan-momentech <nishan@momentum-e.com>
2024-11-19 14:43:35 +0530

    changes pallete in linechart

--- src/app/[locale]/(admin)/home/actionCentre/components/ActionsOverTimeChart.tsx
+++ src/app/[locale]/(admin)/home/actionCentre/components/ActionsOverTimeChart.tsx
@@ -48,9 +48,9 @@ export default function ActionsClosedOverTimeChart({ month = 4, year = 2024 }: {
 
   // Updated color palette with blue shades (to indicate actions being closed)
   const colorPalette = [
-    theme.palette.info.dark,   // Light blue
-    theme.palette.info.main,    // Medium blue
-    theme.palette.info.main,    // Dark blue
+    theme.palette.success.dark,   // Light blue
+    theme.palette.success.main,    // Medium blue
+    theme.palette.success.main,    // Dark blue
   ];
 
   return (
