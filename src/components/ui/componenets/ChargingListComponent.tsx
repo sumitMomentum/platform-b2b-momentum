673a204b67852e368213e399613a8c6a78467108
Himal-Momentum-E <himal@momentum-e.com>
2024-11-22 07:09:31 +0530

    merged dev branch

--- src/components/ui/componenets/ChargingListComponent.tsx
+++ src/components/ui/componenets/ChargingListComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
 
@@ -30,15 +30,26 @@ const columns: GridColDef[] = [
   { field: "DteEnd", headerName: "End DTE", flex: 1 },
   { field: "BatteryAtStart", headerName: "Battery Start (%)", flex: 1 },
   { field: "BatteryAtEnd", headerName: "Battery End (%)", flex: 1 },
-  { field: "DiffInBat", headerName: "Battery Difference (%)", flex: 1, valueGetter: ({ value }) => `${value}%` },
+  {
+    field: "DiffInBat",
+    headerName: "Battery Difference (%)",
+    flex: 1,
+    valueGetter: ({ value }) => `${value}%`,
+  },
   { field: "ChargingType", headerName: "Charging Type", flex: 1 },
   { field: "DiffInDte", headerName: "Difference in DTE", flex: 1 },
   { field: "DwUpdated", headerName: "Last Updated", flex: 1 },
   { field: "chargerId", headerName: "Charger ID", flex: 1 },
 ];
 
-const ChargingList: React.FC<ChargingListProps> = ({ loading, chargingSessions }) => {
-  const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 });
+const ChargingList: React.FC<ChargingListProps> = ({
+  loading,
+  chargingSessions,
+}) => {
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0,
+    pageSize: 10,
+  });
 
   return (
     <Paper sx={{ height: 600, width: "100%" }}>
@@ -47,7 +58,7 @@ const ChargingList: React.FC<ChargingListProps> = ({ loading, chargingSessions }
         rows={chargingSessions}
         columns={columns}
         // initialState={{ pagination: { paginationModel } }}
-          pageSizeOptions={[5, 10]}
+        pageSizeOptions={[5, 10]}
         initialState={{
           pagination: {
             paginationModel: { page: 0, pageSize: 10 },
@@ -55,7 +66,7 @@ const ChargingList: React.FC<ChargingListProps> = ({ loading, chargingSessions }
         }}
         // sx={{ border: 0 }}
         sx={{
-                    backgroundColor: "white",
+          backgroundColor: "white",
 
           ".MuiDataGrid-columnHeaders": {
             fontWeight: "bold",
@@ -65,7 +76,6 @@ const ChargingList: React.FC<ChargingListProps> = ({ loading, chargingSessions }
             fontWeight: "bold", // Ensures header titles specifically are bold
           },
         }}
-
         pagination
         paginationModel={paginationModel}
         onPaginationModelChange={setPaginationModel}
