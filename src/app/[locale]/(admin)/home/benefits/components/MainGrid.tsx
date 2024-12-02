98e260928243050f8b4fdfc94467d5a9242becb6
nishan-momentech <nishan@momentum-e.com>
2024-12-02 14:52:41 +0530

    added overallP&L in serverside and rounded numerical fields to 2 decimal points

--- src/app/[locale]/(admin)/home/benefits/components/MainGrid.tsx
+++ src/app/[locale]/(admin)/home/benefits/components/MainGrid.tsx
@@ -9,19 +9,14 @@ import SavingsDistributionChart from "./SavingsDistributionChart";
 import { getVehicleBenefits } from "@/actions/admin/benefitsListModule/getVehicleBenefits";
 import BenefitsListComponent from "../BenefitsListComponent";
 
-export default function MainGrid({ benefits, loading }) {
+export default function MainGrid({ benefits, overall, loading }) {
   const [statCardData, setStatCardData] = React.useState<StatCardProps[]>([]);
-  const [savingsOverTimeData, setSavingsOverTimeData] = React.useState<
-    number[]
-  >([]);
+  const [savingsOverTimeData, setSavingsOverTimeData] = React.useState<number[]>([]);
   const [vehicleBenefits, setVehicleBenefits] = React.useState([]);
-  // const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
-    // setLoading(true);
     const fetchData = async () => {
       try {
-        // Calculate inferences for stat cards
         const totalSavings = benefits.reduce(
           (acc, item) => acc + item.costSavingChargingMonthly,
           0
@@ -56,7 +51,6 @@ export default function MainGrid({ benefits, loading }) {
           },
         ];
 
-        // Savings data over time for the chart
         const savingsOverTime = benefits.map(
           (item) => item.costSavingChargingMonthly
         );
@@ -64,10 +58,8 @@ export default function MainGrid({ benefits, loading }) {
         setStatCardData(formattedStatCardData);
         setSavingsOverTimeData(savingsOverTime);
         setVehicleBenefits(benefits);
-        // setLoading(false);
       } catch (error) {
         console.error("Failed to fetch data", error);
-        // setLoading(false);
       }
     };
 
@@ -76,43 +68,26 @@ export default function MainGrid({ benefits, loading }) {
 
   return (
     <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
-      {/* Overview Section */}
-      {/* <Typography component="h4" variant="h2" sx={{ mb: 2 }}>
-        Benefits Overview
-      </Typography> */}
-      <Grid
-        container
-        spacing={1}
-        columns={12}
-        sx={{ mb: (theme) => theme.spacing(2) }}
-      >
-        {/* Stat Cards */}
+      <Grid container spacing={1} columns={12} sx={{ mb: (theme) => theme.spacing(2) }}>
         {statCardData.map((card, index) => (
           <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
             <StatCard {...card} loading={loading} />
           </Grid>
         ))}
-
-        {/* Savings Over Time Chart */}
         <Grid item xs={12} md={6} lg={6}>
-          <SavingsOverTimeChart
-            savingsData={savingsOverTimeData}
-            loading={loading}
-          />
+          <SavingsOverTimeChart savingsData={savingsOverTimeData} loading={loading} />
         </Grid>
         <Grid item xs={12} md={6} lg={6}>
-          <SavingsDistributionChart
-            savingsData={vehicleBenefits}
-            loading={loading}
-          />
+          <SavingsDistributionChart savingsData={vehicleBenefits} loading={loading} />
         </Grid>
       </Grid>
-
-      {/* Details Section */}
       <Grid container spacing={2} columns={12}>
-        {/* Data Grid Section */}
         <Grid item xs={12}>
-          <BenefitsListComponent benefits={benefits} loading={loading} />
+          <BenefitsListComponent
+            benefits={benefits}
+            overall={overall}
+            loading={loading}
+          />
         </Grid>
       </Grid>
     </Box>
